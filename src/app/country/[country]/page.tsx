a013919aa840a679e0056ab8660352123521f501
laiba jawed <laib.a.jaweddd@gmail.com>
2024-10-22 23:17:54 +0500

    'Fixes'

--- src/app/country/[country]/page.tsx
+++ src/app/country/[country]/page.tsx
@@ -1,24 +1,33 @@
-import {countriesList} from "../page"
-
-
-export default function CountryName ({params}:{ params:{country:string}}){
-    const selectedCountry = countriesList.find((country) => country.name.toLowerCase() === params.country.toLowerCase());
-    return(
-        <>
-            <div>
-                {selectedCountry  ? (
-                    <div>
-                        <p>Name:{selectedCountry.name}</p>
-                        <p>Population:{selectedCountry.population}</p>
-                        <p>Capital:{selectedCountry.capital}</p>
-                    </div>
-
-                ) :(
-                    <p>Country not found</p>    
-                 
-                )}
-            </div>
-        </>
+type Country = {
+    name: string;
+    population: number;
+    capital: string;
+  }
+  
+  const countriesList: Country[] = [
+    { name: "Pakistan", population: 235800000, capital: "Islamabad" },
+    { name: "India", population: 1417000000, capital: "Delhi" },
+    { name: "Afghanistan", population: 41130000, capital: "Kabul" }
+  ];
+  
+  
+  export default function CountryName({ params }: { params: { country: string } }) {
+  
+    //This is done to localise the imported variable and to prevent any errors occuring during the routing in the URL
+    const selectedCountry = countriesList.find((country) => country.name.toLowerCase() === params.country.toLowerCase()); 
+  
+    return (
+      <div>
+        { // curly bracket (braces) used to enable javascript/typescript so that we can use our variable
+        selectedCountry && ( //I remove the ? because it would complicate the stuff. Anyways we declared our constant here so that we can use it below. Parenthesis () to re-enable HTML
+          <div>
+         
+            <p>Name: {selectedCountry.name}</p> 
+            <p>Capital: {selectedCountry.capital}</p>
+            <p>Population: {selectedCountry.population}</p>
+          </div>
+          ) //closed the HTML here
+        } 
+      </div>
     );
-}
-
+  }
--- src/app/country/page.tsx
+++ src/app/country/page.tsx
@@ -1,15 +1,4 @@
-export interface Country{
-    name: string;
-    population: number;
-    capital: string;
-}
 
-export const countriesList: Country[] =[
-    {name: 'Pakistan', population: 223700000, capital: 'Islamabad'},
-    {name: 'China', population: 1439323776, capital: 'Beijing'},
-    {name: 'India', population: 1380004385, capital: 'New Delhi'}  
- 
-]
 
 
 
